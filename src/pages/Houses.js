4d79457d6c797ee51dffa031bef947931eea57d9
Mike Hu <mikehu@Mikes-MacBook-Pro.local>
2020-10-18 19:00:24 -0400

    my bad

---
+++ src/pages/Houses.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Houses = () => {
+    return (
+        <div>
+            hello from houses
+        </div>
+    )
+}
---
+++ src/pages/Unit.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Unit extends Component {
+    render() {
+        return (
+            <div>
+                hello from unit 
+            </div>
+        )
+    }
+}
