6e8ce0c9f25ee97ca14562022b025d70a5e487a9
Daeseob Lim <thedaysob@gmail.com>
2020-12-23 12:28:38 -0500

    Editing house info feature

--- src/pages/SingleHouse.js
+++ src/pages/SingleHouse.js
@@ -36,6 +36,7 @@ const SingleHouse = props => {
   const [mainImageLink, setMainImageLink] = useState("");
   const [imageLinks, setImageLink] = useState([]);
   const [loadedData, setLoadedData] = useState(false);
+  const [edit, setEdit] = useState(false);
   const [house, setHouse] = useState();
 
   const fetchToken = useCallback(async () => {
@@ -119,6 +120,10 @@ const SingleHouse = props => {
     getHouseInfo();
   }, [fetchToken, getHouseInfo]);
 
+  function onEdit() {
+    console.log("edit");
+    setEdit(true);
+  }
   // const {
   //   averageElecBill,
   //   averageGasBill,
@@ -158,38 +163,51 @@ const SingleHouse = props => {
           </StyledHero>
           <section className="single-room">
             <ImageGallery items={imageLinks} />
-            <div className="single-room-info">
-              <article className="desc">
-                <h3>Full Address</h3>
-                <p>
-                  {" "}
-                  {house.houseaddress}, {house.city}, {house.statename}{" "}
-                  {house.zip}{" "}
-                </p>
-                <br></br>
-                <h3>Contact Info</h3>
-                <p> Landlord Email: {house.landlordemail} </p>
-                <p> Residents Emails: {house.currresidentsemail} </p>
-              </article>
-              <article className="info">
-                <h3>info</h3>
-                <h6>rent: ${house.rent}</h6>
-                <h6>bedrooms: {house.bedrooms}</h6>
-                <h6>bathrooms: {house.bathrooms}</h6>
-                <h6>{house.basement ? "Basement" : "No basement"}</h6>
-                <h6>{house.laundry ? "Laundry" : "No laundry"}</h6>
-                <h6>{house.parking ? "Parking space" : "No parking space"}</h6>
-                <h6>{house.porch ? "Porch" : "No porch"}</h6>
-                <h6>{house.yard ? "Yard" : "No yard"}</h6>
-                <h6>Floor: {house.unit}</h6>
-              </article>
-            </div>
+            {edit ? (
+              <SingleHouseEdit house={house} />
+            ) : (
+              <>
+                {" "}
+                <div className="single-room-info">
+                  <article className="desc">
+                    <h3>Full Address</h3>
+                    <p>
+                      {" "}
+                      {house.houseaddress}, {house.city}, {house.statename}{" "}
+                      {house.zip}{" "}
+                    </p>
+                    {/* <br></br> */}
+                    <h3>Contact Info</h3>
+                    <p> Landlord Email: {house.landlordemail} </p>
+                    <p> Residents Emails: {house.currresidentsemail} </p>
+                  </article>
+                  <article className="info">
+                    <h3>info</h3>
+                    <h6>rent: ${house.rent}</h6>
+                    <h6>bedrooms: {house.bedrooms}</h6>
+                    <h6>bathrooms: {house.bathrooms}</h6>
+                    <h6>{house.basement ? "Basement" : "No basement"}</h6>
+                    <h6>{house.laundry ? "Laundry" : "No laundry"}</h6>
+                    <h6>
+                      {house.parking ? "Parking space" : "No parking space"}
+                    </h6>
+                    <h6>{house.porch ? "Porch" : "No porch"}</h6>
+                    <h6>{house.yard ? "Yard" : "No yard"}</h6>
+                    <h6>Floor: {house.unit}</h6>
+                  </article>
+                  <button onClick={onEdit}>Edit</button>{" "}
+                </div>
+              </>
+            )}
           </section>
           <section className="services-center">
             <HouseComments houseAddress={houseAddress} comments={comments} />
-            <HouseReviewList houseReviews={reviews} />
+            <section>
+              {" "}
+              <HouseReviewList houseReviews={reviews} />
+              <HouseReviewForm houseAddress={houseAddress} />
+            </section>
           </section>
-          <HouseReviewForm houseAddress={houseAddress} />
         </>
       ) : null}
     </div>
@@ -309,3 +327,144 @@ const SingleHouse = props => {
 //   );
 // }
 export default SingleHouse;
+
+const SingleHouseEdit = ({ house }) => {
+  const [landlordEmail, setLandlordEmail] = useState(house.landlordemail);
+  const [houseAddress, setHouseAddress] = useState(house.houseaddress);
+  const [city, setCity] = useState(house.city);
+  const [stateName, setStateName] = useState(house.statename);
+  const [zip, setZip] = useState(house.zip);
+  const [rent, setRent] = useState(house.rent);
+  const [bedrooms, setBedrooms] = useState(house.bedrooms);
+  const [bathrooms, setBathrooms] = useState(house.bathrooms);
+  const [basement, setBasement] = useState(house.basement);
+  const [laundry, setLaundry] = useState(house.laundry);
+  const [parking, setParking] = useState(house.parking);
+  const [porch, setPorch] = useState(house.porch);
+  const [yard, setYard] = useState(house.yard);
+
+  async function onUpdate() {}
+
+  return (
+    <form onSubmit={onUpdate}>
+      <div className="single-room-info">
+        <article className="desc">
+          <h3>Full Address</h3>
+          <p>
+            Street Address:
+            <input
+              type="text"
+              value={houseAddress}
+              onChange={event => setHouseAddress(event.target.value)}
+            />
+          </p>
+          <p>
+            City:
+            <input
+              type="text"
+              value={city}
+              onChange={event => setCity(event.target.value)}
+            />
+          </p>
+          <p>
+            State:
+            <input
+              type="text"
+              value={stateName}
+              onChange={event => setStateName(event.target.value)}
+            />
+          </p>
+          <p>
+            Zip:
+            <input
+              type="text"
+              value={zip}
+              onChange={event => setZip(event.target.value)}
+            />
+          </p>
+          <h3>Contact Info</h3>
+          <p>
+            Landlord Email:
+            <input
+              type="text"
+              value={landlordEmail}
+              onChange={event => setLandlordEmail(event.target.value)}
+            />
+          </p>
+          <p> Residents Emails: {house.currresidentsemail} </p>
+        </article>
+        <article className="info">
+          <h3>info</h3>
+          <h6>
+            Rent:
+            <input
+              type="text"
+              value={rent}
+              onChange={event => setRent(event.target.value)}
+            />
+          </h6>
+          <h6>
+            Bedrooms:
+            <input
+              type="text"
+              value={bedrooms}
+              onChange={event => setBedrooms(event.target.value)}
+            />
+          </h6>
+          <h6>
+            Bathrooms:
+            <input
+              type="text"
+              value={bathrooms}
+              onChange={event => setBathrooms(event.target.value)}
+            />
+          </h6>
+          <h6>
+            Basement:
+            <input
+              type="checkbox"
+              onChange={event => setBasement(event.target.value)}
+            />{" "}
+            {basement ? "    Yes basement" : "    No basement"}
+          </h6>
+          <h6>
+            Laundry:
+            <input
+              type="checkbox"
+              onChange={event => setLaundry(event.target.value)}
+            />{" "}
+            {laundry ? "Yes laundry" : "No laundry"}
+          </h6>{" "}
+          <h6>
+            Parking:
+            <input
+              type="checkbox"
+              onChange={event => setParking(event.target.value)}
+            />{" "}
+            {parking ? "Yes parking" : "No parking"}
+          </h6>
+          <h6>
+            Porch:
+            <input
+              type="checkbox"
+              onChange={event => setPorch(event.target.value)}
+            />{" "}
+            {porch ? "    Yes porch" : "    No porch"}
+          </h6>
+          <h6>
+            Yard:
+            <input
+              type="checkbox"
+              onChange={event => setYard(event.target.value)}
+            />{" "}
+            {yard ? "    Yes yard" : "    No yard"}
+          </h6>
+          <h6>Floor: {house.unit}</h6>
+          <button block bsSize="large" type="submit">
+            Update House Info
+          </button>{" "}
+        </article>
+      </div>
+    </form>
+  );
+};
